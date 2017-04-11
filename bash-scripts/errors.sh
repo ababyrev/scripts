ddc09d2c66930ec8b9371116028dc6126a90a4ff
Alex <ababyrev@gmail.com>
2017-04-11 01:00:48 -0400

    minor changes

--- bash-scripts/errors.sh
+++ bash-scripts/errors.sh
@@ -3,6 +3,6 @@
 printf "ERROR1: Something is wrong! code red\n"
 printf "This is not an error\n"
 printf "ERROR2: Something is very wrong code 321\n"
-printf "ERROR3: Disaster 666"
+printf "ERROR3: Disaster 666\n"
 printf "This is not an error\n"
-printf "ERROR3: More errors\n"
+printf "ERROR4: More errors\n"
--- bash-scripts/fixer.sh
+++ bash-scripts/fixer.sh
@@ -1,38 +1,59 @@
 #!/usr/bin/bash
 
-red=`tput setaf 1`
-green=`tput setaf 2`
-reset=`tput sgr0`
+#Author: Alex Babyrev
+#Date: 04/2017
+#Purpose: Automate error fixing process
 
-if [ ! -f errors.sh ]; then
-	printf "Script not found!\n"
+
+#for colorizing error messages
+ red=`tput setaf 1`
+ green=`tput setaf 2`
+ reset=`tput sgr0`
+
+#Path to script that finds ERRORS
+ ERROR_FINDER="errors.sh"
+
+#Check if the script exists, exit if script is missing
+ if [ ! -f "$ERROR_FINDER" ]; then
+	printf "$ERROR_FINDER script not found!\n"
 	exit 1
-fi
+ fi
 
-# Run to get the errors
-mapfile -t lines < <(bash errors.sh|grep ERROR)
+#Run errors.sh to get output and grep for ERROR lines
+ mapfile -t lines < <(bash errors.sh|grep ERROR)
 
-printf "\n${red}${#lines[@]}${reset} Errors found:\n\n"
-
-printf '%s\n' "${red}${lines[@]}${reset}"
-
-err1_txt="ERROR1: Something is wrong code 1232"
-err2_txt="ERROR2: Something is very wrong code 321"
-err3_txt="ERROR3: Disaster 666"
+#Prints number of errors
+ printf "\n${red}${#lines[@]}${reset} Errors found:\n\n"
 
 
-for i in "${lines[@]}"
-do
+#Print all ERRORS as an array
+ printf '%s\n' "${red}${lines[@]}${reset}"
 
-  if [ "$i" == "$err1_txt" ]
-    then
-      printf "Fixing $i\n"
-    fi
+#Declare variables with ERROR text we're trying to match
+ err1_txt="ERROR1: Something is wrong code 1232"
+ err2_txt="ERROR2: Something is very wrong code 321"
+ err3_txt="ERROR3: Disaster 666"
 
-  if [ "$i" == "$err2_txt" ]
+
+#Loop over every ERROR and if match then do something
+ for i in "${lines[@]}"
+
+  do
+
+   if [ "$i" == "$err1_txt" ]
     then
       printf "\nFixing $i\n"
     fi
 
-done
+   if [ "$i" == "$err2_txt" ]
+    then
+      printf "\nFixing $i\n"
+    fi
+
+   if [ "$i" == "$err3_txt" ]
+    then
+      printf "\nFixing $i\n"
+    fi
+
+ done
 
